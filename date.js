010b543c93b8379459dbd94985f5368dd47f1855
Mariem Amr <62746611+Mariemimix@users.noreply.github.com>
2020-04-19 02:31:21 +0300

    Add files via upload

---
+++ date.js
@@ -0,0 +1,26 @@
+module.exports = {
+  getDay: getDay(),
+  getDate: getDate(),
+}
+
+function getDate() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
+
+function getDay() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
